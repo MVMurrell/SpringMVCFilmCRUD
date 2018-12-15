1b0cd0970a75cb2d34a5d9babfab151573f2d1d1
roguetycoon <michaelmurrell49@gmail.com>
2018-12-14 18:13:38 -0700

    controller stub created

---
+++ src/main/java/com/skilldistillery/filmquery/controller/FilmController.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.filmquery.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FilmController {
+//	A user can enter a Film's ID and see the details of the film in a web page. If the film is not found, they see an appropriate message.
+	
+	
+	
+}
